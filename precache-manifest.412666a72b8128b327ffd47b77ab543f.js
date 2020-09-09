ca4af4c5f4bf721c2833c9194668a6645d58288b
Piyush Deulkar <peeyush.deulkar@yahoo.com>
2020-09-09 11:01:00 +0530

    Updates

---
+++ precache-manifest.412666a72b8128b327ffd47b77ab543f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa75f290cf55fcc34ce7bf321c789b7f",
+    "url": "/ppo-assignment/index.html"
+  },
+  {
+    "revision": "e714f4aec6f07ce6a407",
+    "url": "/ppo-assignment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a830b99da5d813b8a67",
+    "url": "/ppo-assignment/static/js/2.c65339ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ppo-assignment/static/js/2.c65339ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e714f4aec6f07ce6a407",
+    "url": "/ppo-assignment/static/js/main.839a3814.chunk.js"
+  },
+  {
+    "revision": "b329a8c1ca601c27935c",
+    "url": "/ppo-assignment/static/js/runtime-main.12434808.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ppo-assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
