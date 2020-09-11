89717af191d37c74263329d49980aab36cede733
Piyush Deulkar <peeyush.deulkar@yahoo.com>
2020-09-11 17:24:46 +0530

    small correction to prior code commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
 
     onContentSelect = content => {
         this.setState({
-            selectedVideo: video
+            selectedVideo: content
         })
     }
 
